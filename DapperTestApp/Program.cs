bf2678689660ea87b5c70b1e0b3b1e1ce1a37e4a
Fereydoon Karimi <freydoonk@gmail.com>
2017-12-17 19:40:44 +0330

    Initiate commit

---
+++ DapperTestApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Linq;
+using Dapper;
+using SqlServerTypes;
+
+namespace DapperTestApp
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			Utilities.LoadNativeAssemblies(AppDomain.CurrentDomain.BaseDirectory);
+
+			var connection = new SqlConnection("Data Source=.;Initial Catalog=TempDb;User Id=sa;Password=123");
+			var val = connection.Query<Microsoft.SqlServer.Types.SqlHierarchyId>("select @Path", new {Path = Microsoft.SqlServer.Types.SqlHierarchyId.Parse("/1/2/3/")}).Single();
+		}
+	}
+}
